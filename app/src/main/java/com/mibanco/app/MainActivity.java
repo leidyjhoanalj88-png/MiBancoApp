b52dd5fb538228a8371b9c6ef9c3abe1895feb4c
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-03-12 19:16:29 -0500

    Actualizar MainActivity.java

--- app/src/main/java/com/mibanco/app/MainActivity.java
+++ app/src/main/java/com/mibanco/app/MainActivity.java
@@ -1,14 +1,41 @@
 package com.mibanco.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    SessionManager session;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+        super.onCreate(savedInstanceState);
+
+        session = new SessionManager(this);
+
+        if (session.isLogged()) {
+
+            Intent i = new Intent(
+                    MainActivity.this,
+                    DashboardActivity.class
+            );
+
+            startActivity(i);
+
+        } else {
+
+            Intent i = new Intent(
+                    MainActivity.this,
+                    LoginActivity.class
+            );
+
+            startActivity(i);
+
+        }
+
+        finish();
+    }
 }
